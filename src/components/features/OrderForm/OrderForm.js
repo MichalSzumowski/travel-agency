036d8b7122b60f915a6c5348083eee66610a6c57
Michał Szumowski <mic.szumowski@gmail.com>
2021-10-04 12:18:04 +0200

    Add order options from kodilla

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
 //import styles from './OrderForm.scss';
 import {Row, Col} from 'react-flexbox-grid';
+import pricing from '../../../data/pricing.json';
 
 const OrderForm = props => (
 
   <Row>
+    {pricing.map(option => (
+      <Col key={option.id} md={6}>
+        <OrderOption {...option} 
+          currentValue={props.options[option.id]} // ???
+          setOrderOption={props.setOrderOption}
+        />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
     </Col>
@@ -17,6 +27,8 @@ const OrderForm = props => (
 OrderForm.propTypes = {
   tripCost: PropTypes.node,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
+  currentValue: PropTypes.node,
 };
 
 export default OrderForm;
